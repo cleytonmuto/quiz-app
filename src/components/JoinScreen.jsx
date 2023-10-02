c618ff73a0115f579699237f66ef781dc11bda6f
cleytonmuto <cleytonmuto@gmail.com>
2023-10-01 23:37:36 -0300

    init: first commit

---
+++ src/components/JoinScreen.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+const JoinScreen = ({start}) => {
+  return (
+    <div className="join-screen">
+      <h2>Join Quiz</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+      <button onClick={start}>Start</button>
+    </div>
+  );
+}
+
+export default JoinScreen;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="logo">
+            Quiz App
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
